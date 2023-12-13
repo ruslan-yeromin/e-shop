feb2a05166a74b62e195c7bae3723f1d5db7e46d
Ruslan Yeremin <prmoriarty212@gmail.com>
2023-12-13 16:24:04 +0200

    Refactor Navbar, Login, Sign Up, Recovery password pages

--- src/app/(pages)/create-account/CreateAccountForm/index.tsx
+++ src/app/(pages)/create-account/CreateAccountForm/index.tsx
@@ -62,7 +62,8 @@ const CreateAccountForm: React.FC = () => {
         await login(data)
         clearTimeout(timer)
         if (redirect) router.push(redirect as string)
-        else router.push(`/account?success=${encodeURIComponent('Account created successfully')}`)
+        else router.push(`/`)
+      window.location.href = '/'
       } catch (_) {
         clearTimeout(timer)
         setError('There was an error with the credentials provided. Please try again.')
@@ -73,11 +74,6 @@ const CreateAccountForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
-      <p>
-        {`This is where new customers can signup and create a new account. To manage all users, `}
-        <Link href="/admin/collections/users">login to the admin dashboard</Link>
-        {'.'}
-      </p>
       <Message error={error} className={classes.message} />
       <Input
         name="email"
@@ -106,7 +102,7 @@ const CreateAccountForm: React.FC = () => {
       />
       <Button
         type="submit"
-        label={loading ? 'Processing' : 'Create Account'}
+        label={loading ? 'Processing' : 'Sign Up'}
         disabled={loading}
         appearance="primary"
         className={classes.submit}
