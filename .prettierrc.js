71abe2a1027739946a519e467b0003bbab023929
Ruslan Yeremin <prmoriarty212@gmail.com>
2023-12-19 17:01:50 +0200

    fix: build bugs

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,8 @@ module.exports = {
   printWidth: 100,
   parser: 'typescript',
   semi: false,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  endOfLine:"auto"
 }
